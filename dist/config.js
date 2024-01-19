62f86fc3af76c05cc9af0e9c48cfd143e3262118
Flavio <flavionogueirabarros@gmail.com>
2024-01-19 20:36:27 -0300

    remove unused files

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-const dotenv = require("dotenv");
-dotenv.config();
-exports.config = {
-    apiKey: process.env.BBAPIKey,
-};
-//# sourceMappingURL=config.js.map
